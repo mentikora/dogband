68176d1cd082c46270b6e13eae459ea00db4d8e1
Mentikora <notender_boy@ukr.net>
2017-04-05 19:13:19 +0300

    test

--- dist/build.js
+++ dist/build.js
@@ -1,41 +1,41 @@
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-
+/******/
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-
+/******/
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			i: moduleId,
 /******/ 			l: false,
 /******/ 			exports: {}
 /******/ 		};
-
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
+/******/
 /******/ 		// Flag the module as loaded
 /******/ 		module.l = true;
-
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-
-
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-
+/******/
 /******/ 	// identity function for calling harmory imports with the correct context
 /******/ 	__webpack_require__.i = function(value) { return value; };
-
+/******/
 /******/ 	// define getter function for harmory exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
 /******/ 		Object.defineProperty(exports, name, {
@@ -44,7 +44,7 @@
 /******/ 			get: getter
 /******/ 		});
 /******/ 	};
-
+/******/
 /******/ 	// getDefaultExport function for compatibility with non-harmony modules
 /******/ 	__webpack_require__.n = function(module) {
 /******/ 		var getter = module && module.__esModule ?
@@ -53,13 +53,13 @@
 /******/ 		__webpack_require__.d(getter, 'a', getter);
 /******/ 		return getter;
 /******/ 	};
-
+/******/
 /******/ 	// Object.prototype.hasOwnProperty.call
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-
+/******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
@@ -68,13 +68,13 @@
 /* 0 */
 /***/ function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: Cannot find module 'autoprefixer'\n    at Function.Module._resolveFilename (module.js:325:15)\n    at Function.Module._load (module.js:276:25)\n    at Module.require (module.js:353:17)\n    at require (internal/module.js:12:17)\n    at Object.<anonymous> (C:\\Shared\\dogband\\postcss.config.js:3:5)\n    at Module._compile (module.js:409:26)\n    at requireFromString (C:\\Shared\\dogband\\node_modules\\postcss-loader\\node_modules\\postcss-load-config\\node_modules\\cosmiconfig\\node_modules\\require-from-string\\index.js:27:4)\n    at C:\\Shared\\dogband\\node_modules\\postcss-loader\\node_modules\\postcss-load-config\\node_modules\\cosmiconfig\\lib\\loadJs.js:11:15\n    at C:\\Shared\\dogband\\node_modules\\webpack\\lib\\NormalModule.js:143:35\n    at C:\\Shared\\dogband\\node_modules\\webpack\\node_modules\\loader-runner\\lib\\LoaderRunner.js:364:11\n    at C:\\Shared\\dogband\\node_modules\\webpack\\node_modules\\loader-runner\\lib\\LoaderRunner.js:230:18\n    at context.callback (C:\\Shared\\dogband\\node_modules\\webpack\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at C:\\Shared\\dogband\\node_modules\\postcss-loader\\index.js:153:13");
+// removed by extract-text-webpack-plugin
 
 /***/ },
 /* 1 */
 /***/ function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: Cannot find module 'autoprefixer'\n    at Function.Module._resolveFilename (module.js:325:15)\n    at Function.Module._load (module.js:276:25)\n    at Module.require (module.js:353:17)\n    at require (internal/module.js:12:17)\n    at Object.<anonymous> (C:\\Shared\\dogband\\postcss.config.js:3:5)\n    at Module._compile (module.js:409:26)\n    at requireFromString (C:\\Shared\\dogband\\node_modules\\postcss-loader\\node_modules\\postcss-load-config\\node_modules\\cosmiconfig\\node_modules\\require-from-string\\index.js:27:4)\n    at C:\\Shared\\dogband\\node_modules\\postcss-loader\\node_modules\\postcss-load-config\\node_modules\\cosmiconfig\\lib\\loadJs.js:11:15\n    at C:\\Shared\\dogband\\node_modules\\webpack\\lib\\NormalModule.js:143:35\n    at C:\\Shared\\dogband\\node_modules\\webpack\\node_modules\\loader-runner\\lib\\LoaderRunner.js:364:11\n    at C:\\Shared\\dogband\\node_modules\\webpack\\node_modules\\loader-runner\\lib\\LoaderRunner.js:230:18\n    at context.callback (C:\\Shared\\dogband\\node_modules\\webpack\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at C:\\Shared\\dogband\\node_modules\\postcss-loader\\index.js:153:13");
+// removed by extract-text-webpack-plugin
 
 /***/ },
 /* 2 */
